9aafb376b1b10509e04e3ef58ac4991729643160
jukka@pwnlab <jukka@pwnlab.com>
2022-06-10 17:13:37 +0100

    interim commit

--- config/db.js
+++ config/db.js
@@ -3,3 +3,5 @@ const connectDB = async() => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected: ${conn.connection.host}'.cyan.underline.bold);
 };
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ require('dotenv').config();
 
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
