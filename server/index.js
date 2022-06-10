374b223b51f2631ee4f9622908c5b8ba2f86966a
jukka@pwnlab <jukka@pwnlab.com>
2022-06-10 15:41:42 +0100

    interim commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const colors = require('colors');
+
+require('dotenv').config();
+
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}));
+
+app.listen(port, console.log(`server running on port ${port}`));
+
